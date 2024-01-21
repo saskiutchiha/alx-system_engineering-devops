eed8aa7e2cb219e853316ba23983a2a437e00d3d
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-01-21 02:21:46 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,9 +3,9 @@
 
 file {
 '/tmp/school':
-ensure => file,
-mode => '0744',
-owner  => 'www-data',
-group  => 'www-data',
+ensure  => file,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet',
 }
